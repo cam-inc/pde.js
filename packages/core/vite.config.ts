5c9ac16947e26bccbbde3bb884feb90277944306
shuta13 <hollystarsun@gmail.com>
2022-08-19 18:08:33 +0900

    chore: bunchee to vite

---
+++ packages/core/vite.config.ts
@@ -0,0 +1,14 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      formats: ['es', 'cjs'],
+      fileName: (format) => `${format}.js`,
+    },
+    outDir: path.resolve(__dirname, './dist'),
+    emptyOutDir: false,
+  },
+});
