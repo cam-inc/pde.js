6b28d7763217989fe5e46382b66c0740a6d6eeba
shuta13 <hollystarsun@gmail.com>
2022-10-12 13:06:20 +0900

    fix: add license display and sourcemap

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1,14 +1,45 @@
-import {
-  PasteEvent,
-  BlockToolConstructorOptions,
-  InlineToolConstructorOptions,
-  API,
-  ToolConfig,
-  BlockAPI,
-  EditorConfig,
-  BlockToolData,
-} from '@editorjs/editorjs';
-import { BlockTuneData } from '@editorjs/editorjs/types/block-tunes/block-tune-data';
+/**
+ * The implementation included in this file is based on Preact.
+ * Variables, functions, modules, etc. are renamed to names that are easier for us to interpret.
+ *
+ * Here is the files referenced and the original license.
+ *
+ * --- REFERENCED FILES ---
+ *
+ * https://github.com/preactjs/preact/blob/17da4efa736f14c84cd9f36fca4420d94f0dd403/src/jsx.d.ts
+ * https://github.com/preactjs/preact/blob/17da4efa736f14c84cd9f36fca4420d94f0dd403/src/index.d.ts
+ * https://github.com/preactjs/preact/blob/17da4efa736f14c84cd9f36fca4420d94f0dd403/src/internal.d.ts
+ *
+ * --- ---
+ *
+ * --- ORIGINAL LICENSE ---
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015-present Jason Miller
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * --- ---
+ */
+
+import { PasteEvent, EditorConfig, BlockToolData } from '@editorjs/editorjs';
 
 export type ComponentClass<P = {}, _S = {}> = {
   new (props: P, context?: unknown): unknown;
